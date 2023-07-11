59749a46edd0742ec3e6036ea73f8cfc2a196897
Hari <hari@testpress.in>
2023-07-11 21:01:14 +0530

    Add SDK initializer class (#1)

--- lib/tpstreams_player_sdk.dart
+++ lib/tpstreams_player_sdk.dart
@@ -1,3 +1,23 @@
 library tpstreams_player_sdk;
 
 export 'widgets/tp_player.dart';
+
+class TPStreamsSDK {
+  static String? _orgCode;
+
+  static void initialize({required String orgCode}){
+    if (orgCode.isEmpty) {
+      throw Exception("Given OrgCode is empty, please pass a valid orgCode");
+    }
+
+    _orgCode = orgCode;
+  }
+
+  static String get orgCode {
+    if(_orgCode == null) {
+      throw Exception("OrgCode has not been initialized");
+    }
+
+    return _orgCode!;
+  }
+}
