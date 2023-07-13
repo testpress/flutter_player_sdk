b6dfe04bcf5b5b9b87e379f3657af39e8c97ce1e
Hari <hari@testpress.in>
2023-07-13 15:26:11 +0530

    Add support to play Non-DRM video

--- lib/widgets/tp_player.dart
+++ lib/widgets/tp_player.dart
@@ -1,16 +1,88 @@
 import 'package:flutter/material.dart';
+import 'package:better_player/better_player.dart';
+import 'package:tpstreams_player_sdk/services/streams.dart';
+import 'package:tpstreams_player_sdk/tpstreams_player_sdk.dart';
 
-class TPPlayer extends StatelessWidget {
-  const TPPlayer({super.key});
+import '../models/asset.dart';
+
+class TPPlayer extends StatefulWidget {
+  final String assetId;
+  final String accessToken;
+
+  const TPPlayer({
+    Key? key,
+    required this.assetId,
+    required this.accessToken,
+  }) : super(key: key);
+
+  @override
+  State<TPPlayer> createState() => _TPPlayerState();
+}
+
+class _TPPlayerState extends State<TPPlayer> {
+  late BetterPlayerController _controller;
+  late Future<Asset> _assetFuture;
+
+  @override
+  void initState() {
+    super.initState();
+    _assetFuture = fetchAsset(widget.assetId, widget.accessToken);
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text(
-        "Player Widget",
-        style: Theme.of(context).textTheme.headlineMedium,
-        textAlign: TextAlign.center,
+    if (!TPStreamsSDK.isInitialized) {
+      throw Exception(
+          'SDK not initialized. Call TPStreamsSDK.initialize() first.');
+    }
+
+    return FutureBuilder(
+        future: _assetFuture,
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            _controller = getPlayerController(snapshot.data as Asset);
+            return AspectRatio(
+              aspectRatio: 16 / 9,
+              child: BetterPlayer(controller: _controller),
+            );
+          } else if (snapshot.hasError) {
+             return Text(snapshot.error.toString());
+          }
+          else {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+        });
+  }
+
+  BetterPlayerController getPlayerController(Asset asset) {
+    return BetterPlayerController(
+      const BetterPlayerConfiguration(
+        autoPlay: true,
+        aspectRatio: 16 / 9,
+        controlsConfiguration: BetterPlayerControlsConfiguration(
+          enablePlayPause: true,
+          enableFullscreen: true,
+        ),
       ),
+      betterPlayerDataSource: BetterPlayerDataSource(
+          BetterPlayerDataSourceType.network,
+          asset.video.getPlaybackURL()),
     );
+  }
+
+  void pauseVideo() {
+    _controller.pause();
+  }
+
+  void seekTo(Duration position) {
+    _controller.seekTo(position);
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
   }
 }
